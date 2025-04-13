ee00ede346c27cefabadd7d5c2299fd34f80ec14
Jacobo <j.zarruk@uniandes.edu.co>
2025-04-13 18:18:25 -0500

    Creación archivos Taller 1

---
+++ main.ts
@@ -0,0 +1,5 @@
+// import { Serie } from './serie.js';
+// import { data } from './data.js';
+
+let series: HTMLElement = document.getElementById('series')!;
+let serieSeleccionada: HTMLElement = document.getElementById('serieSeleccionada')!;
