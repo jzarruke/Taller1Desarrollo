3fd5666b7433f83f33aee8cd6fe4b71805dc19d0
Jacobo <j.zarruk@uniandes.edu.co>
2025-04-13 22:02:35 -0500

    Página terminada

--- main.ts
+++ main.ts
@@ -1,5 +1,31 @@
-// import { Serie } from './serie.js';
-// import { data } from './data.js';
+import { Serie } from './series.js';
+import { series } from './data.js';
 
-let series: HTMLElement = document.getElementById('series')!;
+let seriesTbody: HTMLElement = document.getElementById('series')!;
 let serieSeleccionada: HTMLElement = document.getElementById('serieSeleccionada')!;
+
+renderSeriesInTable(series);
+renderAverageSeasons(series);
+
+function renderSeriesInTable(series: Serie[]) {
+    series.forEach((serie) => {
+        let trElement = document.createElement("tr");
+        trElement.classList.add("table-active");
+        trElement.innerHTML = `<td scope="row">${serie.id}</td>
+                                <td>${serie.name}</td>
+                                <td>${serie.channel}</td>
+                                <td>${serie.seasons}</td>`;
+        seriesTbody.appendChild(trElement);
+    });
+}
+
+function renderAverageSeasons(series: Serie[]) {
+    let totalSeasons = 0;
+    let trElement = document.createElement("tr");
+    series.forEach((serie) => {
+        totalSeasons += serie.seasons;
+    });
+    let averageSeasons = totalSeasons / series.length;
+    trElement.innerHTML = `<td colspan="3">Seasons average: ${averageSeasons}</td>`;
+    seriesTbody.appendChild(trElement);
+}
---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+import { series } from './data.js';
+var seriesTbody = document.getElementById('series');
+var serieSeleccionada = document.getElementById('serieSeleccionada');
+renderSeriesInTable(series);
+renderAverageSeasons(series);
+function renderSeriesInTable(series) {
+    series.forEach(function (serie) {
+        var trElement = document.createElement("tr");
+        trElement.classList.add("table-active");
+        trElement.innerHTML = "<td scope=\"row\">".concat(serie.id, "</td>\n                                <td>").concat(serie.name, "</td>\n                                <td>").concat(serie.channel, "</td>\n                                <td>").concat(serie.seasons, "</td>");
+        seriesTbody.appendChild(trElement);
+    });
+}
+function renderAverageSeasons(series) {
+    var totalSeasons = 0;
+    var trElement = document.createElement("tr");
+    series.forEach(function (serie) {
+        totalSeasons += serie.seasons;
+    });
+    var averageSeasons = totalSeasons / series.length;
+    trElement.innerHTML = "<td colspan=\"3\">Seasons average: ".concat(averageSeasons, "</td>");
+    seriesTbody.appendChild(trElement);
+}
